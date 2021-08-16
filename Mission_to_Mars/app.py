c03aa9c49fc215b090f96ec327a9ac8a778e004e
Breckin_Shoemaker <breckinshoemaker@gmail.com>
2021-08-16 15:15:25 -0500

    Pushing updated code

--- Mission_to_Mars/app.py
+++ Mission_to_Mars/app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
 import scrape_mars
-import pymongo
 
 
 app = Flask(__name__)
@@ -33,6 +32,7 @@ def scraper():
 
     # Redirect back to home page
     return redirect("/", code=302)
+
 
 
 if __name__ == "__main__":
